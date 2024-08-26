057f2fc470c048b115d796b2717b62ff9320ea93
nathanchow <nathannokhangc2027@student.cis.edu.hk>
2024-08-26 08:52:28 +0800

    Finished First Lab

--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner ageInput = new Scanner(System.in);
+        System.out.println ("What is your age?");
+        int age = ageInput.nextInt();
+        int pagesToRead = 100 - age;
+        System.out.println ("You should read " + pagesToRead + " pages before putting down a book.");
 
     }
+
 }
